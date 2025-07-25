664265fc4c29ff1c89fa51ee1c36b506bdaa9deb
Thompson Falowo <thompsongboyega2019@gmail.com>
2025-07-26 00:09:44 +0100

    Modified

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -3,6 +3,8 @@ from django.db import models
 # Create your models here.
 class Author(models.Model):
     name = models.CharField(max_length=255)
+    def __str__(self):
+        return self.name
 
 class Book(models.Model):
     title = models.CharField(max_length=255)
