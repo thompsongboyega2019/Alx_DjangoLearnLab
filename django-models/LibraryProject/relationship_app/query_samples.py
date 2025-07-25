5e569ae456e55fd4f7a9b10cef429af6afd552c2
Thompson Falowo <thompsongboyega2019@gmail.com>
2025-07-26 00:14:23 +0100

    Modified

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,5 +1,6 @@
 from models import Book, Author, Librarian, Library
 
 books_by_author = Book.objects.filter(author__name='Author Name')
-books_in_library = Book.objects.filter(library__name='Main City Library')
+library = Library.objects.get(name='Main City Library')
+books_in_library = library.books.all()
 librarian = Library.objects.get(name='Main City Library').librarian
