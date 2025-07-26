8b79fd46cb55b2c7438793f67fcc11bad4b72a78
Thompson Falowo <thompsongboyega2019@gmail.com>
2025-07-26 20:16:47 +0100

    Okay

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,4 +4,4 @@ from .models import Book
 # Create your views here.
 def listbooks(request):
     book_list = Book.objects.all()
-    return render(request, 'list_books.html', {'books': book_list} )
+    return render(request, 'relationship_app/list_books.html', {'books': book_list} )
