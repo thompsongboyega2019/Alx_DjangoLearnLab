4bcb1362019b2caace1cfb6bc1588ae753445e0f
Thompson Falowo <thompsongboyega2019@gmail.com>
2025-07-27 19:11:55 +0100

    Class based View added

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView
-from .models import Book, Library
+from .models import Library, Book
 
 # Create your views here.
 def listbooks(request):
