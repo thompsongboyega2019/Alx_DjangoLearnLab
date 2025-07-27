147128d4f2721e6ee82185517c498435d507157a
Thompson Falowo <thompsongboyega2019@gmail.com>
2025-07-27 21:13:33 +0100

    Done

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,14 +1,16 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView, SignUpView
+# from .views import list_books, LibraryDetailView, SignUpView
+from . import views
 from django.contrib.auth.views import LoginView, LogoutView
 
 
 
 urlpatterns = [
-    path('books', list_books, name="list_of_books"),
-    path('register', SignUpView.as_view(), name='register'),
-    path('library', LibraryDetailView.as_view(), name='library'  ),
+    # path('books', list_books, name="list_of_books"),
+    # path('register', SignUpView.as_view(), name='register'),
+    # path('library', LibraryDetailView.as_view(), name='library'  ),
+    path('register', views.register, name='register'),
     path('login', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
-    path('', LogoutView.as_view(), name='logout'),
+    path('', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
     
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.detail import DetailView
 from .models import Library, Book
 from django.contrib.auth.forms import UserCreationForm
-from django.urls import reverse_lazy
-from django.views.generic import CreateView
 from django.contrib.auth import login
+# from django.urls import reverse_lazy
+# from django.views.generic import CreateView
+
 
 
 # Create your views here.
@@ -19,7 +20,18 @@ class LibraryDetailView(DetailView):
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
 
-class SignUpView(CreateView):
-    form_class = UserCreationForm
-    success_url = reverse_lazy('relationship_app/login.html')
-    template_name = 'relationship_app/register.html'
+# class SignUpView(CreateView):
+#     form_class = UserCreationForm
+#     success_url = reverse_lazy('relationship_app/login.html')
+#     template_name = 'relationship_app/register.html'
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()  # Save user to DB
+            login(request, user)  # Log the user in immediately
+            return redirect('relationship_app/login.html')  # Redirect to a post-login page
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})
