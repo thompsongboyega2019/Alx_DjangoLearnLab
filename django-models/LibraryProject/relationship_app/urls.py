a525e1f2731cde030a64a0a5179e1f6a8a9d5ee5
Thompson Falowo <thompsongboyega2019@gmail.com>
2025-07-27 19:10:18 +0100

    Class-based Views Added

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('books', views.listbooks, name="list_of_books"),
+    path('library', views.LibraryDetailsView.as_view(), name='library'  )
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
-from .models import Book
+from django.views.generic import ListView
+from .models import Book, Library
 
 # Create your views here.
 def listbooks(request):
     book_list = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': book_list} )
+
+
+
+class LibraryDetailsView(ListView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
