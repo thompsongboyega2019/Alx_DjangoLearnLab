997021d57dea0bd3695a26029c75b2a212103bec
Thompson Falowo <thompsongboyega2019@gmail.com>
2025-08-03 00:19:42 +0100

    Okay

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,10 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
-class Book(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.CharField(max_length=200)
-    published_year = models.IntegerField()
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to='', null=True, blank=True)
+
+# Create your models here.
+# class Book(models.Model):
+#     title = models.CharField(max_length=200)
+#     author = models.CharField(max_length=200)
+#     published_year = models.IntegerField()
     
     
 
--- advanced_features_and_security/LibraryProject/relationship_app/models.py
+++ advanced_features_and_security/LibraryProject/relationship_app/models.py
@@ -1,10 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
-class CustomUser(AbstractUser):
-    date_of_birth = models.DateField(null=True, blank=True)
-    profile_photo = models.ImageField(upload_to='', null=True, blank=True)
 
 # # Create your models here.
 # class Author(models.Model):
