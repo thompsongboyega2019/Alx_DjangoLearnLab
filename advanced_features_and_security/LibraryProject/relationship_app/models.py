838a8b5729bc44a5b6e14f812609fd5c04f989f4
Thompson Falowo <thompsongboyega2019@gmail.com>
2025-08-03 00:13:09 +0100

    Okay

--- advanced_features_and_security/LibraryProject/relationship_app/models.py
+++ advanced_features_and_security/LibraryProject/relationship_app/models.py
@@ -1,27 +1,34 @@
 from django.db import models
 
 
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField()
+    profile_photo = models.ImageField()
 
 # # Create your models here.
-class Author(models.Model):
-    name = models.CharField(max_length=255)
-    def __str__(self):
-        return self.name
+# class Author(models.Model):
+#     name = models.CharField(max_length=255)
+#     def __str__(self):
+#         return self.name
 
-class Book(models.Model):
-    title = models.CharField(max_length=255)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+# class Book(models.Model):
+#     title = models.CharField(max_length=255)
+#     author = models.ForeignKey(Author, on_delete=models.CASCADE)
 
 
-    class Meta:
-        permissions = [
-            ("can_add_book", "Can add book"),
-            ("can_change_book", "Can change book"),
-            ("can_delete_book", "Can delete book"),
-        ]
+#     class Meta:
+#         permissions = [
+#             ("can_add_book", "Can add book"),
+#             ("can_change_book", "Can change book"),
+#             ("can_delete_book", "Can delete book"),
+#         ]
     
-    def __str__(self):
-        return self.title
+#     def __str__(self):
+#         return self.title
 
 # class Library(models.Model):
 #     name = models.CharField(max_length=255)
