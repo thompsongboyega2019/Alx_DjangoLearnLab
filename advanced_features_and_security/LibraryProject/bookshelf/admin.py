7c3e3436ea6c181045732fd3f89fa00fc93f08e4
Thompson Falowo <thompsongboyega2019@gmail.com>
2025-08-03 00:24:17 +0100

    Okay

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,13 +1,32 @@
 from django.contrib import admin
-from .models import Book
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-@admin.register(Book)
-class BookAdmin(admin.ModelAdmin):
-    # Fields to display in the admin list view
-    list_display = ('title', 'author', 'published_year')
+class CustomUserAdmin(UserAdmin):
+    # Optional: customize how your user appears in admin,
+    # for example adding fields like date_of_birth and profile_photo
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
 
-    # Enable filtering by publication_year
-    list_filter = ('published_year',)
+admin.site.register(CustomUser, CustomUserAdmin)
 
-    # Enable search functionality for title and author fields
-    search_fields = ('title', 'author')
+
+
+
+# from django.contrib import admin
+# from .models import Book
+
+# @admin.register(Book)
+# class BookAdmin(admin.ModelAdmin):
+#     # Fields to display in the admin list view
+#     list_display = ('title', 'author', 'published_year')
+
+#     # Enable filtering by publication_year
+#     list_filter = ('published_year',)
+
+#     # Enable search functionality for title and author fields
+#     search_fields = ('title', 'author')
