c69ed6e1054639130c34b647482c96f560eb4bb4
Thompson Falowo <thompsongboyega2019@gmail.com>
2025-08-03 16:28:04 +0100

    okay

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -2,5 +2,7 @@
 
 from django import forms
 
-class SearchForm(forms.Form):
-    query = forms.CharField(required=True, max_length=100)
+class ExampleForm(forms.Form):
+    title = forms.CharField(label='Book Title', max_length=100)
+    author = forms.CharField(label='Author Name', max_length=100)
+    published_date = forms.DateField(label='Published Date', widget=forms.DateInput(attrs={'type': 'date'}))
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,14 +1,20 @@
 # bookshelf/views.py
 
-from django.shortcuts import render
-from django.db.models import Q
+from django.shortcuts import render, redirect
+from .forms import ExampleForm
 from .models import Book
-from .forms import SearchForm  # ✅ Correct import
 
-def book_search(request):
-    form = SearchForm(request.GET)
-    books = []
-    if form.is_valid():
-        query = form.cleaned_data['query']
-        books = Book.objects.filter(Q(title__icontains=query) | Q(author__icontains=query))
-    return render(request, 'bookshelf/book_list.html', {'books': books, 'form': form})
+def book_create(request):
+    if request.method == 'POST':
+        form = ExampleForm(request.POST)
+        if form.is_valid():
+            Book.objects.create(
+                title=form.cleaned_data['title'],
+                author=form.cleaned_data['author'],
+                published_date=form.cleaned_data['published_date']
+            )
+            return redirect('book_list')
+    else:
+        form = ExampleForm()
+
+    return render(request, 'bookshelf/book_form.html', {'form': form})
