5cb2a44b94394ac6f9cc438acca00db2a43e3ff7
Thompson Falowo <thompsongboyega2019@gmail.com>
2025-08-03 08:25:48 +0100

    Okay

--- advanced_features_and_security/LibraryProject/relationship_app/urls.py
+++ advanced_features_and_security/LibraryProject/relationship_app/urls.py
@@ -1,25 +1,3 @@
-# from django.urls import path
-# # from .views import list_books, LibraryDetailView, SignUpView
-# from . import views
-# from django.contrib.auth.views import LoginView, LogoutView
-
-
-
-# urlpatterns = [
-#     # path('books', list_books, name="list_of_books"),
-#     # path('register', SignUpView.as_view(), name='register'),
-#     # path('library', LibraryDetailView.as_view(), name='library'  ),
-#     path('register', views.register, name='register'),
-#     path('login', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
-#     path('', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
-#     path('admin/', views.admin_view, name='admin_view'),
-#     path('librarian/', views.librarian_view, name='librarian_view'),
-#     path('member/', views.member_view, name='member_view'),
-    
-    
-# ]
-
-
 
 from django.urls import path
 from . import views
