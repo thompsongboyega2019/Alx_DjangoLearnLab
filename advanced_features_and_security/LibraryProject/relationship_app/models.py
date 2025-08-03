6075bfe9d5ed669c8350f69454412b454023f3a6
Thompson Falowo <thompsongboyega2019@gmail.com>
2025-08-03 15:47:55 +0100

    Okay

--- advanced_features_and_security/LibraryProject/relationship_app/models.py
+++ advanced_features_and_security/LibraryProject/relationship_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    published_at = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        permissions = [
+            ("can_view", "Can view article"),
+            ("can_create", "Can create article"),
+            ("can_edit", "Can edit article"),
+            ("can_delete", "Can delete article"),
+        ]
+
+    def __str__(self):
+        return self.title
--- advanced_features_and_security/LibraryProject/relationship_app/views.py
+++ advanced_features_and_security/LibraryProject/relationship_app/views.py
@@ -1,61 +1,36 @@
+# myapp/views.py
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import permission_required
-from django.contrib import messages
-from django.urls import reverse
-from .models import Book
-from .form import BookForm  # Assuming you have a BookForm
+from .models import Article
 
-def book_list(request):
-    """View to display all books - no permission required for viewing"""
-    books = Book.objects.all()
-    return render(request, 'relationship_app/book_list.html', {'books': books})
+@permission_required('myapp.can_view', raise_exception=True)
+def article_list(request):
+    articles = Article.objects.all()
+    return render(request, 'myapp/article_list.html', {'articles': articles})
 
-@permission_required('relationship_app.can_add_book')
-def add_book(request):
-    """View to add a new book - requires can_add_book permission"""
+@permission_required('myapp.can_create', raise_exception=True)
+def article_create(request):
     if request.method == 'POST':
-        form = BookForm(request.POST)
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'Book added successfully!')
-            return redirect('book_list')
-    else:
-        form = BookForm()
-    
-    return render(request, 'relationship_app/add_book.html', {'form': form})
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        Article.objects.create(title=title, content=content)
+        return redirect('article_list')
+    return render(request, 'myapp/article_form.html')
 
-@permission_required('relationship_app.can_change_book')
-def edit_book(request, book_id):
-    """View to edit an existing book - requires can_change_book permission"""
-    book = get_object_or_404(Book, id=book_id)
-    
+@permission_required('myapp.can_edit', raise_exception=True)
+def article_edit(request, pk):
+    article = get_object_or_404(Article, pk=pk)
     if request.method == 'POST':
-        form = BookForm(request.POST, instance=book)
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'Book updated successfully!')
-            return redirect('book_list')
-    else:
-        form = BookForm(instance=book)
-    
-    return render(request, 'relationship_app/edit_book.html', {
-        'form': form, 
-        'book': book
-    })
+        article.title = request.POST.get('title')
+        article.content = request.POST.get('content')
+        article.save()
+        return redirect('article_list')
+    return render(request, 'myapp/article_form.html', {'article': article})
 
-@permission_required('relationship_app.can_delete_book')
-def delete_book(request, book_id):
-    """View to delete a book - requires can_delete_book permission"""
-    book = get_object_or_404(Book, id=book_id)
-    
+@permission_required('myapp.can_delete', raise_exception=True)
+def article_delete(request, pk):
+    article = get_object_or_404(Article, pk=pk)
     if request.method == 'POST':
-        book.delete()
-        messages.success(request, 'Book deleted successfully!')
-        return redirect('book_list')
-    
-    return render(request, 'relationship_app/delete_book.html', {'book': book})
-
-def book_detail(request, book_id):
-    """View to display book details - no permission required for viewing"""
-    book = get_object_or_404(Book, id=book_id)
-    return render(request, 'relationship_app/book_detail.html', {'book': book})
+        article.delete()
+        return redirect('article_list')
+    return render(request, 'myapp/article_confirm_delete.html', {'article': article})
