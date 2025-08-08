462876214e62e357ac3ff7358d65f1fd8bb5709f
Thompson Falowo <thompsongboyega2019@gmail.com>
2025-08-08 23:55:02 +0100

    Setup a new Django project with Django REST Framework

--- advanced_features_and_security/LibraryProject/bookshelf/groups.py
+++ advanced_features_and_security/LibraryProject/bookshelf/groups.py
@@ -1,4 +1,4 @@
-# bookshelf/management/commands/setup_groups.py
+
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import Group, Permission
 from django.contrib.contenttypes.models import ContentType
