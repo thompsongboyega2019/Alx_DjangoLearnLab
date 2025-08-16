c9951bf96996447c565c25c3ba7949cf94831db1
Thompson Falowo <thompsongboyega2019@gmail.com>
2025-08-16 19:39:48 +0100

    Create a new Django project called django_app

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -75,8 +75,13 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_app',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'USER': 'root',
+        'PASSWORD': '@Lokoja123$',  # Change this to your MySQL password
+        'PORT': '3306',  
     }
 }
 
