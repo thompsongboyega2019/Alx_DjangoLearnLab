a3330960ac4cf8d117e2be50cf2a5d2ee6b04b79
Thompson Falowo <thompsongboyega2019@gmail.com>
2025-08-16 19:59:49 +0100

    Fixed

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,6 +1,6 @@
     # Comment CRUD
-    path('post/<int:post_id>/comments/new/', views.CommentCreateView.as_view(), name='comment-create'),
-    path('comment/<int:pk>/edit/', views.CommentUpdateView.as_view(), name='comment-update'),
+    path('post/<int:pk>/comments/new/', views.CommentCreateView.as_view(), name='comment-create'),
+    path('comment/<int:pk>/update/', views.CommentUpdateView.as_view(), name='comment-update'),
     path('comment/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='comment-delete'),
 from django.urls import path
 from . import views
