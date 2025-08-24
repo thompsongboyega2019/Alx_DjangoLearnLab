f3eea9c1bca806854eb35e846e5f5ef4da3bed48
Thompson Falowo <thompsongboyega2019@gmail.com>
2025-08-24 19:10:43 +0100

    Created the Social Media Api project and Account App

---
+++ social_media_api/accounts/views.py
@@ -0,0 +1,37 @@
+
+
+from rest_framework import generics, permissions
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from django.contrib.auth import get_user_model
+from .serializers import RegisterSerializer, UserSerializer
+
+User = get_user_model()
+
+class RegisterView(generics.CreateAPIView):
+	queryset = User.objects.all()
+	serializer_class = RegisterSerializer
+
+	def create(self, request, *args, **kwargs):
+		response = super().create(request, *args, **kwargs)
+		user = self.object if hasattr(self, 'object') else self.get_object()
+		token, created = Token.objects.get_or_create(user=user)
+		response.data['token'] = token.key
+		return response
+
+class CustomAuthToken(ObtainAuthToken):
+	def post(self, request, *args, **kwargs):
+		serializer = self.serializer_class(data=request.data, context={'request': request})
+		serializer.is_valid(raise_exception=True)
+		user = serializer.validated_data['user']
+		token, created = Token.objects.get_or_create(user=user)
+		return Response({'token': token.key, 'user_id': user.pk, 'username': user.username})
+
+class ProfileView(generics.RetrieveUpdateAPIView):
+	queryset = User.objects.all()
+	serializer_class = UserSerializer
+	permission_classes = [permissions.IsAuthenticated]
+
+	def get_object(self):
+		return self.request.user
