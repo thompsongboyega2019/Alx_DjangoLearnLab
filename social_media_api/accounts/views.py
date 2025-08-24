2edbf3369bccf705bb76722363ab91a3e5521871
Thompson Falowo <thompsongboyega2019@gmail.com>
2025-08-24 19:53:51 +0100

    The follow and unfollow endpoints have been added accordingly

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,3 +1,24 @@
+from rest_framework.views import APIView
+from django.shortcuts import get_object_or_404
+class FollowUserView(APIView):
+	permission_classes = [permissions.IsAuthenticated]
+
+	def post(self, request, user_id):
+		user_to_follow = get_object_or_404(CustomUser, id=user_id)
+		if user_to_follow == request.user:
+			return Response({'detail': 'You cannot follow yourself.'}, status=400)
+		request.user.following.add(user_to_follow)
+		user_to_follow.followers.add(request.user)
+		return Response({'detail': f'You are now following {user_to_follow.username}.'})
+
+class UnfollowUserView(APIView):
+	permission_classes = [permissions.IsAuthenticated]
+
+	def post(self, request, user_id):
+		user_to_unfollow = get_object_or_404(CustomUser, id=user_id)
+		request.user.following.remove(user_to_unfollow)
+		user_to_unfollow.followers.remove(request.user)
+		return Response({'detail': f'You have unfollowed {user_to_unfollow.username}.'})
 
 
 from rest_framework import generics, permissions
